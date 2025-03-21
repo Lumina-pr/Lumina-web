d4e4371c389bd9ccb0f67f254676fb8b3250db6d
Miguel <miguelangel2005.o.h@gmail.com>
2025-03-20 21:56:08 -0500

    refactor: reorganize styles and components; add new UI elements for login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,5 @@
-import Image from "next/image";
+import LoginTemplate from "@/components/templates/LoginTemplate";
 
-export default function Login() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-6">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm">
-        <div className="flex justify-center mb-6">
-          <Image
-            src="/next.svg"
-            alt="Logo"
-            width={100}
-            height={50}
-          />
-        </div>
-        <h2 className="text-2xl font-bold text-center text-gray-700 mb-4">
-          Iniciar Sesión
-        </h2>
-        <form>
-          <div className="mb-4">
-            <label className="block text-gray-600 text-sm mb-2">Correo Electrónico</label>
-            <input
-              type="email"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 text-black placeholder-gray-500"
-              placeholder="correo@ejemplo.com"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-600 text-sm mb-2">Contraseña</label>
-            <input
-              type="password"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 text-black placeholder-gray-500"
-              placeholder="********"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-200"
-          >
-            Iniciar Sesión
-          </button>
-        </form>
-        <p className="text-sm text-center text-gray-600 mt-4">
-          ¿No tienes una cuenta? <a href="#" className="text-blue-500 hover:underline">Regístrate</a>
-        </p>
-      </div>
-    </div>
-  );
+export default function Home() {
+  return <LoginTemplate />;
 }
---
+++ src/components/atoms/Input.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface InputProps {
+  id?: string;  
+  type: string;
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input: React.FC<InputProps> = ({ id, type, placeholder, value, onChange }) => {
+  return (
+    <input
+      id={id}  
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className="border bg-customBlue text-xs border-transparent px-4 shadow-lg py-2 rounded-lg w-full focus:border-yellow-500 focus:ring-1 focus:ring-yellow-500 text-black"
+    />
+  );
+};
+
+export default Input;
