b53334b3fdb2d1fcdb89c8ce18c4148561d640ab
Sebastian-Ag <sebastian.agudelos@unac.edu.co>
2025-03-28 11:01:01 -0500

    refactor: remove unused LoginTemplate import from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import LoginTemplate from "@/components/templates/LoginTemplate";
 import RegisterTemplate from "@/components/templates/RegisterTemplate";
 export default function Home() {
   return <RegisterTemplate />;
